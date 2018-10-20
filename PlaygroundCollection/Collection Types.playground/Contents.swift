53baa571318f9d54d0d27e59c332c5984199fddc
mert <mertnesvat@gmail.com>
2018-10-20 11:29:22 +0300

    ✅ collection types

--- PlaygroundCollection/Collection Types.playground/Contents.swift
+++ PlaygroundCollection/Collection Types.playground/Contents.swift
@@ -1,3 +1,171 @@
-import UIKit
+// Collection Types
 
-var str = "Hello, playground"
+//Swift provides three primary collection types, known as
+// arrays,
+// sets,
+// dictionaries.
+
+//For more information about using Array with Foundation and Cocoa, see Bridging Between Array and NSArray.
+
+var someInts = [Int]()
+var someOtherInts = Array<Int>()
+print("someInts is of type [Int] with \(someInts.count) also \(someOtherInts.count) items.")
+// Prints "someInts is of type [Int] with 0 items."
+
+someInts.append(3)
+// someInts now contains 1 value of type Int
+someInts = []
+// someInts is now an empty array, but is still of type [Int]
+
+print(someInts)
+
+//Default Value
+var threeDoubles = Array(repeating: 0.0, count: 3)
+// threeDoubles is of type [Double], and equals [0.0, 0.0, 0.0]
+
+// + add arrays
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+// anotherThreeDoubles is of type [Double], and equals [2.5, 2.5, 2.5]
+
+var sixDoubles = threeDoubles + anotherThreeDoubles
+// sixDoubles is inferred as [Double], and equals [0.0, 0.0, 0.0, 2.5, 2.5, 2.5]
+
+var shoppingList = ["Eggs", "Milk", "Flour"]
+
+shoppingList += ["Baking Powder", "Chocolate Spread", "Cheese", "Butter"]
+
+shoppingList[4...6] = ["Bananas", "Apples"]
+// shoppingList now contains 6 items
+
+shoppingList
+shoppingList.insert("Maple Syrup", at: 0)
+
+let mapleSyrup = shoppingList.remove(at: 0)
+// the item that was at index 0 has just been removed
+// shoppingList now contains 6 items, and no Maple Syrup
+// the mapleSyrup constant is now equal to the removed "Maple Syrup" string
+shoppingList
+
+for item in shoppingList {
+    print(item)
+}
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
+
+// Sets
+
+// A type must be hashable in order to be stored in a set
+
+var letters = Set<Character>()
+print("letters is of type Set<Character> with \(letters.count) items.")
+// Prints "letters is of type Set<Character> with 0 items."
+
+letters.insert("a")
+// letters now contains 1 value of type Character
+letters = []
+// letters is now an empty set, but is still of type Set<Character>
+
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+
+if favoriteGenres.contains("Funk") {
+    print("I get up on the good foot.")
+} else {
+    print("It's too funky in here.")
+}
+// Prints "It's too funky in here."
+
+for genre in favoriteGenres.sorted() {
+    print("\(genre)")
+}
+
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let singleDigitPrimeNumbers: Set = [2, 3, 5, 7]
+
+oddDigits.union(evenDigits).sorted()
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+oddDigits.intersection(evenDigits).sorted()
+// []
+oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
+// [1, 9]
+oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted()
+// [1, 2, 9]
+
+let houseAnimals: Set = ["🐶", "🐱"]
+let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "🐱"]
+let cityAnimals: Set = ["🐦", "🐭"]
+
+houseAnimals.isSubset(of: farmAnimals)
+// true
+farmAnimals.isSuperset(of: houseAnimals)
+// true
+farmAnimals.isDisjoint(with: cityAnimals)
+// true
+
+// Dictionaries
+
+// A dictionary Key type must conform to the Hashable protocol, like a set’s value type.
+
+var namesOfIntegers = [Int: String]()
+// namesOfIntegers is an empty [Int: String] dictionary
+
+namesOfIntegers[16] = "sixteen"
+// namesOfIntegers now contains 1 key-value pair
+namesOfIntegers = [:]
+// namesOfIntegers is once again an empty dictionary of type [Int: String]
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+// Prints "The old value for DUB was Dublin."
+
+if let oldValue = airports.updateValue("Istanbul Airport", forKey: "IST") {
+    print("No update this time it's added!")
+}
+airports
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName).")
+} else {
+    print("That airport is not in the airports dictionary.")
+}
+
+airports["APL"] = "Apple International"
+// "Apple International" is not the real airport for APL, so delete it
+airports["APL"] = nil
+// APL has now been removed from the dictionary
+
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+// Prints "The removed airport's name is Dublin Airport."
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+// YYZ: Toronto Pearson
+// LHR: London Heathrow
+
+let airportCodes = [String](airports.keys)
+// airportCodes is ["YYZ", "LHR"]
+
+let airportNames = [String](airports.values)
+// airportNames is ["Toronto Pearson", "London Heathrow"]
+
+let airportCodes2 = Array<String>(airports.keys) // different syntax
+
+airports.sorted { (first, second) -> Bool in
+    return first.key >= second.key
+}
+
+
+
+
+
+
+
